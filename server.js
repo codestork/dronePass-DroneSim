8afe14210004a7c4303fee65faa33994fca54bab
Dennis Lin <pioneerlike@gmail.com>
2015-01-02 17:01:55 -0800

    [fix] delete hard-coded test

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
 var LineStep = require('./utils/lineStep');
-
-var ls1 = LineStep([ [1,3],
-                     [5,7],
-                     [10,-19],
-                         ] );
-
-console.log(ls1.step(1.5));
-console.log(ls1.step(2));
-console.log(ls1.step(3));
