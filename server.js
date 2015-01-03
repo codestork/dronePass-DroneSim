67412af45caa17a0a59c10f1013499836b017ba5
Dennis Lin <pioneerlike@gmail.com>
2015-01-02 22:15:24 -0800

    (feature) basic functions

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var turf = require('turf');
-var drone1 = require('./droneSim');
+var droneSim = require('./droneSim');
 
+var drone1 = droneSim.makeDrone();
 drone1.takeOff();
 drone1.fly();
-setTimeout(function(){
-  drone1.stop.apply(drone1);
-},3500)
+
+ setTimeout(function(){
+   // drone1.stop.apply(drone1);
+   drone1.changeRoute.call(drone1, 1,[[-5,-5],[-8,-8]]);
+ },2000)
