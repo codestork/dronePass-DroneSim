48fa42700226645cb6412886bb807fe2019f317a
Dennis Lin <pioneerlike@gmail.com>
2015-01-05 18:55:52 -0800

    (feature) drone communication basics

--- droneSim.js
+++ droneSim.js
@@ -4,28 +4,30 @@ var LineStep = require('./utils/lineStep');
 
 var droneSim = {
   // drone constructor
-  makeDrone: function(path, speed) {
+  makeDrone: function(callSign, path, speed) {
     // `drone` object to export
     var drone = {};
+    drone.callSign = callSign || "VX-seven oh two";
     path = path || 
             [
-            [1,3],
-            [3,19],
-            [20,25],
-            [26,-75],
-            [-55,122],
-            [60,122],
+              [-122.251911, 37.866316],
+              [-122.25500106811523, 37.86401247373357],
+              [-122.2723388671875, 37.86347038587407],
+              [-122.271137,37.854356],
+              [-122.28667259216309,37.852119505594274],
+              [-122.27993488311768,37.83103950459009],
             ];
 
     // `speed` indicate the speed of the drone, represented in m/s
-    speed = speed || 10;
+    drone.speed = drone.speed || 0.0001;
 
     var ls = LineStep(path);
 
     // if `takenOff` is `false` means the drone was landed
-    var takenOff = false;
+    drone.takenOff = false;
     // the drone would only "fly" forward
-    var isFlying = false;
+    drone.isFlying = false;
+    drone.isArrived = false;
 
     // To increase precision, update every 0.1s
     var computeInterval = 100;
@@ -33,47 +35,71 @@ var droneSim = {
     // Core computation to simulate drone flying state
     var compute = function() {
       // a step is calculated based on computation cycle interval
-      _step = droneSim.fluc(speed,-80,80)/(1000/computeInterval);
+      _step = droneSim.fluc(drone.speed,-80,80)/(1000/computeInterval);
       // a done can only be flying when it is in the sky
-      if ( takenOff && isFlying ) ls.step(_step);
+      if ( drone.takenOff && drone.isFlying ) ls.step(_step);
 
+      if ( ls.currPoint[0] === path[path.length-1][0] )
       // random fluctuation added to current location and rounded up to .2f
       ls.currPoint = [droneSim.fluc(ls.currPoint[0],-10,10), droneSim.fluc(ls.currPoint[1],-10,10)];
-      ls.currPoint = [Math.round(ls.currPoint[0]*100)/100, Math.round(ls.currPoint[1]*100)/100];
+      // ls.currPoint = [Math.round(ls.currPoint[0]*100)/100, Math.round(ls.currPoint[1]*100)/100];
     }
 
     drone.intervalHandler_compute = setInterval(compute, computeInterval);
 
 
-    
+    drone.getStatusCode = function() {
+      if ( drone.isArrived ) {
+        return 10;
+      }
+      else if ( !drone.takenOff && !drone.isFlying ) {
+        // statusCode 0: at stand at ground, waiting for taking off
+        return 0;
+      } else if ( drone.takenOff && !drone.isFlying ) {
+        // statusCode 1: hovering in sky, waiting for instruction or new plan
+        return 1;
+      } else if ( drone.takenOff && drone.isFlying ) {
+        // statusCode 2: flying in sky, following instruction or preset plan
+        return 2;
+      }
+
+      // statusCode 199: Exception, Hazard, SOS
+      return 199;
+    }
 
     drone.takeOff = function() {
-      takenOff = true;
+      drone.takenOff = true;
     }
 
     drone.land = function() {
-      takenOff = false;
+      drone.takenOff = false;
     }
 
     drone.fly = function() {
-      isFlying = true;
+      drone.isFlying = true;
     }
 
     drone.stop = function() {
-      isFlying = false;
+      drone.isFlying = false;
     }
+
 
     // get an object representation of the state of the drone
     drone.getCurrentState = function() {
-      return { location: ls.currPoint,
+      return { callSign: drone.callSign,
+
+               // location: [Math.round(ls.currPoint[0]*100)/100, Math.round(ls.currPoint[1]*100)/100],
+               location: [ls.currPoint[0], ls.currPoint[1]],
+               speed: drone.speed,
                prevPathPtInd: ls.prevCtrlPtInd,
-               distance: ls.currDist
+               distance: Math.round(ls.currDist*100)/100,
+               statusCode: drone.getStatusCode()
              };
     }
 
     // set the speed of drone
     drone.setSpeed = function( newSpeed ) {
-      speed = newSpeed;
+      drone.speed = newSpeed;
     }
 
     drone.changeRoute = function( pivotPointInd, substitute ) {
@@ -87,17 +113,12 @@ var droneSim = {
       ls.step(dist);
     }
 
-    // ------- debug logger --------
-    setInterval(function(){
-      console.log(drone.getCurrentState());
-    }, 1000);
-
     return drone;
   },
   // Generalized fluctuation between [m,n], AWARE unit in cm/s
   fluc: function(input, m, n) {
-    var d = Random.integer(m,n)/100;
-    input += d;
+    //var d = Random.integer(m,n)/100;
+    //input += d;
     return input;
   }
 }
--- server.js
+++ server.js
@@ -1,11 +1,149 @@
 var turf = require('turf');
 var droneSim = require('./droneSim');
+var fs = require('fs');
+var Random = require("random-js")();
 
-var drone1 = droneSim.makeDrone();
-drone1.takeOff();
-drone1.fly();
 
- setTimeout(function(){
-   // drone1.stop.apply(drone1);
-   drone1.changeRoute.call(drone1, 1,[[-5,-5],[-8,-8]]);
- },2000)
+var drone1 = droneSim.makeDrone('Mike-Lima-November-9-1-7');
+
+ setInterval(function(){
+    console.log(drone1.getCurrentState());
+  }, 1000);
+
+var rndWords = function() {
+  var len = arguments.length;
+  var ind = Random.integer(0,len-1);
+  return arguments[ind];
+}
+
+var io = require('socket.io')(8080);
+
+io
+  .on('connection', function (socket) {
+
+    socket.on('reportIn', function(msg) {
+      // example -> { location: [ 2.05, 12.14 ], prevPathPtInd: 0, distance: 8.999 }
+      var report = drone1.getCurrentState();
+      report.transcript = drone1.callSign + 
+                          " loc " + report.location[0] + ", " + report.location[1] + " . " +
+                          "PPI " + report.prevPathPtInd + " . " +
+                          "distance " + report.distance + 
+                          rndWords(' from base.', " from home.", "");
+
+      socket.emit('update', report);
+    });
+
+    socket.on('takeOff', function(msg) {
+      var report = drone1.getCurrentState();
+
+      if ( drone1.isFlying ) {
+        report.transcript = drone1.callSign + ' copy.' +
+                            ' we were flying, will keep going';
+      } else {
+        report.transcript = rndWords('this is ', '') + drone1.callSign + ' taking off,'+ 
+                            rndWords(' confirm.', ' good day.', ' good flight.');
+        drone1.takeOff();
+        drone1.fly();
+      }
+
+      socket.emit('ack', report);
+    });
+
+    socket.on('setSpeed', function(msg) {
+      var report = drone1.getCurrentState();
+
+      if ( msg.newSpeed === 0 ) {
+
+      }
+
+      if ( !drone1.isFlying ) {
+        report.transcript = rndWords('this is ', '') + drone1.callSign +
+                            rndWords(', we were idle.') +
+                            rndWords(' waiting for new instruction');
+      } else {
+        report.transcript = rndWords('this is ', '') + drone1.callSign +
+                            rndWords(' acknowledged,', ' copy,', ' understood,') +
+                            rndWords(' maintaining', ' changing to', ' in') +
+                            " speed " + msg.newSpeed + ' .';
+        drone1.setSpeed(msg.newSpeed);
+      }
+
+      socket.emit('ack', report);
+    });
+
+    socket.on('fly', function(msg) {
+      var report = drone1.getCurrentState();
+      if ( drone1.isFlying ) {
+        report.transcript = drone1.callSign + ' copy.' +
+                                    ' we were flying, will keep going';
+      } 
+      else if ( !drone1.isFlying && !drone1.takenOff ) {
+        report.transcript = rndWords('this is ', '') + drone1.callSign +
+                            rndWords(' at ground ', '') +
+                            rndWords(', need clearance for', ', waiting for') +
+                            " taking off";
+      }
+      else if ( !drone1.isFlying && drone1.takenOff ) {
+        report.transcript = rndWords('this is ', '') + drone1.callSign +
+                            rndWords(' acknowledged,', ' copy,', ' understood,') +
+                            rndWords(' fly ahead', ' head straight', ' going');
+        drone1.fly();
+      }
+
+      socket.emit('ack', report);
+      
+    });
+
+    socket.on('stop', function(msg) {
+      var report = drone1.getCurrentState();
+
+      if ( !drone1.isFlying && !drone1.takenOff ) {
+        report.transcript = rndWords('this is ', '') + drone1.callSign +
+                            rndWords(' at stand,', ' copy,') +
+                            rndWords(' we were holding', ' keeping idle') +
+                            " at ground";
+      }
+      else if ( !drone1.isFlying && drone1.takenOff ) {
+        report.transcript = rndWords('this is ', '') + drone1.callSign +
+                            rndWords(' at stand,', ' copy,') +
+                            rndWords(' we were stopping', ' keeping idle') +
+                            " in sky";
+      } else {
+        report.transcript = rndWords('this is ', '') + drone1.callSign +
+                            rndWords(' at stand,', ' copy,', ' understood,') +
+                            rndWords(' stopping now', ' pausing around', ' idle now') +
+                            " in sky";
+      }
+
+      drone1.stop();
+      socket.emit('ack', report);
+    });
+
+  socket.on('land', function(msg) {
+    var report = drone1.getCurrentState();
+
+    if ( !drone1.takenOff ) {
+      report.transcript = rndWords('this is ', '') + drone1.callSign +
+                          rndWords(' at stand,', ' copy,') +
+                          rndWords(' we were holding', ' keeping idle') +
+                          " at ground";
+    } else {
+      report.transcript = rndWords('this is ', '') + drone1.callSign +
+                          rndWords(' at stand,', ' copy,', ' understood,') +
+                          rndWords(' landing now', ' going down' );
+    }
+
+    drone1.stop();
+    drone1.land();
+    socket.emit('ack', report);
+  });
+
+    socket.on('changeRoute', function(msg) {
+      var report = drone1.getCurrentState();
+      report.transcript = rndWords('this is ', '') + drone1.callSign +
+                          rndWords(' reporting in,', ' copy,', ' pivoting,') +
+                          rndWords(' new path received', ' following new path', ' incoming new plan accepted');
+      drone1.changeRoute( msg.pivotPointInd, msg.substitutePath );
+      socket.emit('ack', report);
+    });
+  });
